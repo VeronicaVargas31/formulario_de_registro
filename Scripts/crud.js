c801207d0e85f3d5e0566be6ce2ce7bb2d2ed1dd
VeronicaVargas31 <veronica.vargas@syscomcolombia.com>
2024-11-29 13:57:02 -0500

    Reestructuracion de carpetas

--- Scripts/crud.js
+++ Scripts/crud.js
@@ -1,8 +1,50 @@
+$(document).ready(function() {
+
+    $.ajax({
+        url: 'PHP/list.php',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            renderTable(data);
+        },
+        error: function() {
+            console.error("Error al obtener los datos.");
+        }
+    });
+
+function renderTable(data) {
+    let tableBody = '';
+    data.forEach(user => {
+        tableBody += `
+            <tr>
+                <td>${user.formulario_de_registro_id}</td>
+                <td>${user.nombre_completo}</td>
+                <td>${user.genero}</td>
+                <td>${user.fecha_nacimiento}</td>
+                <td>${user.pais}</td>
+                <td>${user.ciudad}</td>
+                <td>${user.ocupacion}</td>
+                <td>${user.direccion}</td>
+                <td>${user.email}</td>
+                <td>${user.phone}</td>
+                <td><button class="btn-editar" data-id="${user.formulario_de_registro_id}">Editar</button></td>
+                <td><button class="btn btn-outline-dark" onclick="deleteUser(${user.formulario_de_registro_id})">Eliminar</button></td>
+            </tr>
+        `;
+    });
+    $('#tbody').html(tableBody);
+}
+})
+
+
+
+//obtener usuario y actualizar
+
 $(document).on('click', '.btn-editar', function() {
     var userId = $(this).data('id'); 
    
     $.ajax({
-        url: 'obtener_usuario.php', 
+        url: 'PHP/obtener_usuario.php', 
         method: 'GET',
         data: { id: userId }, 
         success: function(response) {
@@ -20,8 +62,6 @@ $(document).on('click', '.btn-editar', function() {
                 $('#direccion').val(usuario.direccion);
                 $('#email').val(usuario.email);
                 $('#phone').val(usuario.phone);
-
-                // Mostrar el formulario de edición
                 $('#formEditar').show();
             } catch (error) {
                 console.error("Error al analizar la respuesta del servidor: ", error);
@@ -35,22 +75,12 @@ $(document).on('click', '.btn-editar', function() {
     });
 });
 
-
-
-
-
-
-
-
-
-
-
 $('#formEditar').submit(function(e) {
     e.preventDefault();
 
     var formData = $(this).serialize(); 
     $.ajax({
-        url: 'actualizar.php', 
+        url: 'PHP/actualizar.php', 
         type: 'POST', 
         data: formData, 
         dataType: 'json',
@@ -72,5 +102,25 @@ $('#formEditar').submit(function(e) {
 
 
 
-
-
+//Borrar 
+function deleteUser(userId) {
+    if (confirm('¿Estás seguro de que deseas eliminar este usuario?')) {
+        $.ajax({
+        url: 'PHP/borrar.php', 
+        type: 'POST',
+        data: { id: userId }, 
+        dataType: 'json' ,
+        success: function(response) {
+            if (response.success) {
+             
+             alert('Usuario eliminado correctamente'); 
+            } else {
+                alert('Hubo un error al eliminar el usuario');
+                }
+            },
+            error: function() {
+                console.error("Error al eliminar el usuario.");
+            }
+        });
+    }
+}
